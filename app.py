706412e69310379fd424d99187bf58e020854eaa
liuzhuan <liuz_geek@163.com>
2024-09-25 16:35:25 +0800

    feat: add bullets

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import pygame
+from pygame.sprite import Group
 from settings import Settings
 from ship import Ship
 import game_functions as gf
@@ -10,10 +11,12 @@ def run_game():
     pygame.display.set_caption('Alien Invasion')
 
     ship = Ship(game_settings, screen)
+    bullets = Group()
 
     while True:
-        gf.check_events(ship)
+        gf.check_events(game_settings, screen, ship, bullets)
         ship.update()
-        gf.update_screen(game_settings, screen, ship)
+        bullets.update()
+        gf.update_screen(game_settings, screen, ship, bullets)
 
 run_game()
--- game_functions.py
+++ game_functions.py
@@ -1,11 +1,15 @@
 import pygame
 import sys
+from bullet import Bullet
 
-def check_keydown_events(event, ship):
+def check_keydown_events(event, setting, screen, ship, bullets):
     if event.key == pygame.K_RIGHT:
         ship.moving_right = True
     elif event.key == pygame.K_LEFT:
         ship.moving_left = True
+    elif event.key == pygame.K_SPACE:
+        new_bullet = Bullet(setting, screen, ship)
+        bullets.add(new_bullet)
 
 def check_keyup_events(event, ship):
     if event.key == pygame.K_RIGHT:
@@ -13,17 +17,19 @@ def check_keyup_events(event, ship):
     elif event.key == pygame.K_LEFT:
         ship.moving_left = False
 
-def check_events(ship):
+def check_events(setting, screen, ship, bullets):
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.KEYDOWN:
-            check_keydown_events(event, ship)
+            check_keydown_events(event, setting, screen, ship, bullets)
         elif event.type == pygame.KEYUP:
             check_keyup_events(event, ship)
 
 
-def update_screen(settings, screen, ship):
+def update_screen(settings, screen, ship, bullets):
     screen.fill(settings.bg_color)
+    for bullet in bullets.sprites():
+        bullet.draw_bullet()
     ship.blitme()
     pygame.display.flip()
