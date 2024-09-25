ff6a67fef4aa8d9a441f8441e544a39985c0ca0e
liuzhuan <liuz_geek@163.com>
2024-09-25 22:46:23 +0800

    feat: add click handler for play button

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from pygame.sprite import Group
 from settings import Settings
 from ship import Ship
 from game_stats import GameStats
+from button import Button
 import game_functions as gf
 
 def run_game():
@@ -10,6 +11,7 @@ def run_game():
     game_settings = Settings()
     screen = pygame.display.set_mode(game_settings.size)
     pygame.display.set_caption('Alien Invasion')
+    play_button = Button(game_settings, screen, 'Play')
     stats = GameStats(game_settings)
 
     ship = Ship(game_settings, screen)
@@ -19,12 +21,12 @@ def run_game():
     gf.create_fleet(game_settings, screen, ship, aliens)
 
     while True:
-        gf.check_events(game_settings, screen, ship, bullets)
+        gf.check_events(game_settings, screen, stats, play_button, ship, bullets)
         if stats.game_active:
             ship.update()
             gf.update_bullets(game_settings, screen, ship, aliens, bullets)
             gf.update_aliens(game_settings, stats, screen, ship, aliens, bullets)
 
-        gf.update_screen(game_settings, screen, ship, aliens, bullets)
+        gf.update_screen(game_settings, screen, stats, ship, aliens, bullets, play_button)
 
 run_game()
