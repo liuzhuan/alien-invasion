6a80f522f269b2305cf0467eac5e62f66bd6869a
liuzhuan <liuz_geek@163.com>
2024-09-25 16:39:04 +0800

    feat: remove bullets out of screen

--- app.py
+++ app.py
@@ -17,6 +17,11 @@ def run_game():
         gf.check_events(game_settings, screen, ship, bullets)
         ship.update()
         bullets.update()
+
+        for bullet in bullets.copy():
+            if bullet.rect.bottom <= 0:
+                bullets.remove(bullet)
+
         gf.update_screen(game_settings, screen, ship, bullets)
 
 run_game()
